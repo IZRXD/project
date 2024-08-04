73adcdec74179f4fde6c5f02ace143fbddd0cf30
Borislav Mihaylov <borvalorant@gmail.com>
2024-08-04 21:31:41 +0300

    added download button

--- client/src/components/wallpaper-list/wallpaper-list-item/WallpaperListItem.jsx
+++ client/src/components/wallpaper-list/wallpaper-list-item/WallpaperListItem.jsx
@@ -1,22 +1,7 @@
-// import { Link } from "react-router-dom";
 
+import DownloadLink from "react-download-link"
 export default function WallpaperListItem(wallpaper) {
-  const downloadFIleAtUrl = (url) => {
-    console.log(url);
-    
-    fetch(url)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const blobURL = window.URL.createObjectURL(new Blob([blob]));
-        const fileName = url.split("/").pop();
-        const aTag = document.createElement("a");
-        aTag.href = blobURL;
-        aTag.setAttribute("download", fileName);
-        document.body.appendChild(aTag);
-        aTag.click();
-        aTag.remove();
-      });
-  };
+
   return (
     <div className="row pt-4 gx-0 mb-5 mb-lg-0 justify-content-center">
       <div className="col-lg-6">
@@ -31,12 +16,15 @@ export default function WallpaperListItem(wallpaper) {
           <div className="d-flex h-100">
             <div className="project-text w-100 my-auto text-center text-lg-left">
               <h4 className="text-white">{wallpaper.title}</h4>
-              <a
-                className="btn"
-                onClick={downloadFIleAtUrl(wallpaper.image)}
-              >
-                Download
-              </a>
+              <DownloadLink
+                label="Download Image"
+                filename="image.jpg"
+                exportFile={() =>
+                  fetch(wallpaper.image).then((res) =>
+                    res.blob()
+                  )
+                }
+              />
             </div>
           </div>
         </div>
